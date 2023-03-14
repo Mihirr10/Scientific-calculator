669e9020b3c1e5873631b6ff2dcd07a75179ca8e
mihir <mihirparmar031@gmail.com>
2023-03-14 09:53:17 +0530

    made changes and added comments

--- javascript.js
+++ javascript.js
@@ -1,207 +1,240 @@
-let display=document.getElementById('result');
-   
+let display = document.getElementById('result');
 
-function dis(val){
- 
+//function for displaying values
+function dis(val) {
+
     display.value += val;
-   
+
+
+}
+//function for radian to degree
+function deg() {
+    display.value = ((180 / Math.PI) * display.value).toFixed(2);
+}
+
+//function for fixed-exponential
+function fe() {
+    display.value = Number.parseFloat(display.value).toExponential();
+}
+
+//function for memory clear
+
+function mc() {
+    localStorage.removeItem("memory");
+    document.getElementById("memoryClear").disabled = true;
+    document.getElementById("memoryRecall").disabled = true;
 
 }
 
-function sresult(){
- 
-    let x=eval(display.value);
-    display.value=x;
-
+//function for memory recall
+function mr() {
+    display.value = localStorage.getItem("memory");
 }
 
-function del(){
-  
-    display.value="";
+//function for memory plus
+function mPlus() {
+    display.value = eval(localStorage.getItem("memory") + "+" + display.value);
+    localStorage.setItem("memory", display.value);
+    document.getElementById("memoryClear").disabled = false;
+    document.getElementById("memoryRecall").disabled = false;
 }
 
-function rem(){
-  
-    display.value=display.value.substring(0,display.value.length-1);
-
+//function for memory minus
+function mMinus() {
+    display.value = eval(localStorage.getItem("memory") + "-" + display.value);
+    localStorage.setItem("memory", display.value);
+    document.getElementById("memoryClear").disabled = false;
+    document.getElementById("memoryRecall").disabled = false;
+}
+//function for memory store
+function ms() {
+    localStorage.setItem("memory", display.value);
+    document.getElementById("memoryClear").disabled = false;
+    document.getElementById("memoryRecall").disabled = false;
 }
 
-function sqrt(){
-   
-    display.value=Math.sqrt(display.value);
-}
+//function for trogonometry
+function trigonometry() {
+    let a = document.getElementById("trigo").value;
 
-function square(){
-    display.value=Math.pow(display.value,2);
-}
-
-function log(){
-    display.value=Math.log10(display.value);
-}
-
-function ln(){
-    display.value=Math.log(display.value);
-}
-
-function trigonometry(){
-  let a =document.getElementById("trigo").value;
-
-  if( a == 'sinx'){
-    sin();
-  }
-  else if( a == 'cosx')
-  {
-    cos();
-  }else if( a == 'tanx')
-  {
-    tan();
-  }
-  
-}
-
-function functions(){
-    let b =document.getElementById("functions").value;
-  
-    if( b == 'abs'){
-      abs();
+    if (a == 'sinx') {
+        sin();
     }
-    else if( b == 'floor')
-    {
-      floor();
-    }else if( b == 'ceil')
-    {
-      ceil();
+    else if (a == 'cosx') {
+        cos();
+    } else if (a == 'tanx') {
+        tan();
     }
-    
-  }
 
-
-function abs(){
-    display.value=Math.abs(display.value);
 }
 
-function floor(){
-    display.value=Math.floor(display.value);
+//function for sin value
+function sin() {
+    display.value = Math.sin(display.value * Math.PI / 180).toFixed(2);
+
 }
 
-function ceil(){
-    display.value=Math.ceil(display.value);
+//function for cos value
+
+function cos() {
+    display.value = Math.cos(display.value * Math.PI / 180).toFixed(2);
 }
 
-function sin(){
-    display.value=Math.sin(display.value * Math.PI/180).toFixed(2);
-    
+//function for tan value
+
+function tan() {
+    display.value = Math.tan(display.value * Math.PI / 180).toFixed(2);
 }
 
-function cos(){
-    display.value=Math.cos(display.value * Math.PI/180).toFixed(2);
+
+//function for functions
+
+function functions() {
+    let b = document.getElementById("functions").value;
+
+    if (b == 'abs') {
+        abs();
+    }
+    else if (b == 'floor') {
+        floor();
+    } else if (b == 'ceil') {
+        ceil();
+    }
+
+}
+//function for floor value
+function floor() {
+    display.value = Math.floor(display.value);
 }
 
-function tan(){
-    display.value=Math.tan(display.value * Math.PI/180).toFixed(2);
+//function for ceil value
+function ceil() {
+    display.value = Math.ceil(display.value);
 }
 
-function exp(){
-    display.value=Math.exp(display.value);
+
+//function for twotoN
+function twoToN() {
+    display.value = Math.pow(2, display.value);
+}
+//function for pi
+function pi() {
+    const pi = 3.14159265359;
+
+    if (display.value === '') {
+        display.value = pi;
+    }
+    else {
+        display.value = display.value * pi;
+    }
 }
 
-function factorial(){
-    var result=1;
+//function for exponential
+function e() {
+    const e = 2.718281828459045;
 
-    if(display.value === 0){
+    if (display.value === '') {
+        display.value = e;
+    }
+    else {
+        display.value = display.value * e;
+    }
+}
+
+
+// function for maths operations
+function sresult() {
+
+    let x = eval(display.value);
+    display.value = x;
+
+}
+
+//function for clear the screen
+function del() {
+
+    display.value = "";
+}
+
+//function for backspace
+function remove() {
+
+    display.value = display.value.substring(0, display.value.length - 1);
+
+}
+
+//function for square
+function square() {
+    display.value = Math.pow(display.value, 2);
+}
+
+//function for inverse
+function inverse() {
+    display.value = 1 / display.value;
+
+}
+
+//function for removing the minus sign
+function abs() {
+    display.value = Math.abs(display.value);
+}
+
+//function for exp
+function exp() {
+    display.value = Math.exp(display.value);
+}
+//function for square root
+function squareRoot() {
+
+    display.value = Math.sqrt(display.value);
+}
+
+
+
+//function for factorial
+function factorial() {
+    var result = 1;
+
+    if (display.value === 0) {
         display.value = "1";
     }
-    else if(display.value === 1){
+    else if (display.value === 1) {
         display.value = "not defined";
     }
 
     else {
-        var result=1;
-        for(var i=display.value ;i>0 ;i--){
-            result =result * i;
+        var result = 1;
+        for (var i = display.value; i > 0; i--) {
+            result = result * i;
         }
 
-        display.value=result;
+        display.value = result;
     }
 }
 
-function plusminus(){
-    if(display.value.charAt(0) === '-'){
-        display.value=display.value.slice(-1);
+//function for log
+function log() {
+    display.value = Math.log10(display.value);
+}
+//finction for ln
+function ln() {
+    display.value = Math.log(display.value);
+}
+
+//function for plusminus
+function plusMinus() {
+    if (display.value.charAt(0) === '-') {
+        display.value = display.value.slice(-1);
     }
-    else{
-        display.value="-"+display.value;
-    }
-}
-
-function inverse(){
-    display.value=1/display.value;
-}
-
-function power(){
-    display.value=Math.pow(10,display.value);
-}
-
-function twoToN(){
-    display.value=Math.pow(2,display.value);
-}
-
-function pi(){
-    const pi=3.14159265359;
-
-    if(display.value === ''){
-        display.value=pi;
-    }
-    else{
-        display.value=display.value*pi;
+    else {
+        display.value = "-" + display.value;
     }
 }
 
-function e(){
-    const e=2.718281828459045;
 
-    if(display.value === ''){
-        display.value=e;
-    }
-    else{
-        display.value=display.value*e;
-    }
-}
-
-function deg(){
-    display.value=((180/Math.PI)*display.value).toFixed(2);
-}
-
-function fe(){
-    display.value=Number.parseFloat(display.value).toExponential();
-}
-
-function mc(){
-    localStorage.removeItem("memory");
-}
-
-function mr(){
-     display.value=localStorage.getItem("memory");
-}
-
-function mPlus(){
-    display.value=eval(localStorage.getItem("memory") + "+" + display.value);
-    localStorage.setItem("memory",display.value);
-    document.getElementById("memoryClear").disabled=false;
-    document.getElementById("memoryRecall").disabled=false;
-}
-
-function mMinus(){
-    display.value=eval(localStorage.getItem("memory") + "-" + display.value);
-    localStorage.setItem("memory",display.value);
-    document.getElementById("memoryClear").disabled=false;
-    document.getElementById("memoryRecall").disabled=false;
-}
-
-function ms(){
-    localStorage.setItem("memory",display.value);
-    document.getElementById("memoryClear").disabled=false;
-    document.getElementById("memoryRecall").disabled=false;
+//function for power of 10
+function power() {
+    display.value = Math.pow(10, display.value);
 }
 
 
